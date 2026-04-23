8490996dd4c1bf9cde814c04afe8416ed34b3c3b
jackan04 <jacob.asker@icloud.com>
2026-04-23 19:48:19 +0200

    feat: add passport middleware and configure express to parse URL data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import passport from "./config/passport.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
 
 app.get("/", (_req, res) => {
   res.json({ message: "Hello, world!" });
