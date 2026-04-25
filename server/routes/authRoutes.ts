e6de41989254a0aef82cc2c3b00efd249d6df4d8
jackan04 <jacob.asker@icloud.com>
2026-04-25 13:12:51 +0200

    feat: add autenticate middleware to the getMe route

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, register, getMe } from "../controllers/authController.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
-authRouter.get("/me", getMe);
+authRouter.get("/me", authenticate, getMe);
 
 export default authRouter;
