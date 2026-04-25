f1d7f963e616363d37eccb573f45770cad2d0182
jackan04 <jacob.asker@icloud.com>
2026-04-25 12:45:56 +0200

    feat: mount userRouter in main router

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRoutes.js";
+import userRouter from "./userRoutes.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 export default router;
