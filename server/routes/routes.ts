f8d04d0c8d61362b312210af336badb9345d2659
jackan04 <jacob.asker@icloud.com>
2026-04-25 13:12:34 +0200

    feat: add authentication and authorization to user routes

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRoutes.js";
 import userRouter from "./userRoutes.js";
+import { authenticate } from "../middleware/authenticate.js";
+import { authorize } from "../middleware/authorize.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/user", userRouter);
+router.use("/user", authenticate, authorize, userRouter);
 
 export default router;
