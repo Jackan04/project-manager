1647d241de2c38ed309c33e47503a56bb973d84c
jackan04 <jacob.asker@icloud.com>
2026-04-25 08:13:41 +0200

    feat: add cors configuration

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,12 @@ import "dotenv/config";
 import passport from "./config/passport.js";
 import errorHandler from "./middleware/errorHandler.js";
 import notFoundHandler from "./middleware/notFoundHandler.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
