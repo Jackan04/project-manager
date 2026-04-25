6126e63d8bb3ad449ada8c005882bb1c09c01323
jackan04 <jacob.asker@icloud.com>
2026-04-25 07:49:56 +0200

    feat: import and use errorHandler and notFoundHandler middleware

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import passport from "./config/passport.js";
+import errorHandler from "./middleware/errorHandler.js";
+import notFoundHandler from "./middleware/notFoundHandler.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -8,10 +10,8 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
-
-app.get("/", (_req, res) => {
-  res.json({ message: "Hello, world!" });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
